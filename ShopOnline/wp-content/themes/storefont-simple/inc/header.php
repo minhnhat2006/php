171afacfd32c772ef6d2ab9e823c222929344d08
Nathan Nguyen <minhnhat2006@gmail.com>
2017-09-11 17:24:37 +0700

    Update ShopOnline

---
+++ ShopOnline/wp-content/themes/storefont-simple/inc/header.php
@@ -0,0 +1,25 @@
+<?php
+
+// Polylang Shortcode - https://wordpress.org/plugins/polylang/
+// Add this code in your functions.php
+// Put shortcode [polylang] to post/page for display flags
+
+function polylang_shortcode() {
+	ob_start();
+	pll_the_languages(array('show_flags'=>1,'show_names'=>1));
+	$flags = ob_get_clean();
+	return $flags;
+}
+add_shortcode( 'polylang', 'polylang_shortcode' );
+
+
+add_filter( 'wp_nav_menu_items', 'single_custom_menu_item', 10, 2 );
+
+function single_custom_menu_item ( $items, $args ) {
+
+    if ($args->theme_location == 'primary') {
+        $items .= do_shortcode('[polylang]');
+    }
+
+    return $items;
+}
